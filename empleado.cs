79c818274465d19e1d21b70aa329fd84f6b86c5d
guadalupe atim <guadiiatim1309@gmail.com>
2023-06-01 19:42:06 -0300

    atributos

--- empleado.cs
+++ empleado.cs
@@ -1,13 +1,15 @@
 namespace Empleados;
 
 public class Empleado{
-    private string nombre;
-    private string apellido;
+
+    //atributos
+    private string nombre = String.Empty;
+    private string apellido = String.Empty;
     private DateTime fechadenacimiento;
     private char estadocivil;
     private DateTime fechaingreso;
     private double sueldobasico;
-    private string cargo;
+    private Cargos cargo;
 
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apellido { get => apellido; set => apellido = value; }
@@ -15,7 +17,22 @@ public class Empleado{
     public char Estadocivil { get => estadocivil; set => estadocivil = value; }
     public DateTime Fechaingreso { get => fechaingreso; set => fechaingreso = value; }
     public double Sueldobasico { get => sueldobasico; set => sueldobasico = value; }
-    public string Cargo { get => cargo; set => cargo = value; }
+    //internal Cargos Cargo { get => cargo; set => cargo = value; }
+    //metodos
+
+    public int edad;
+    public int antiguedad;
+    
+    void Antiguedad(DateTime Fechaingreso){
+        antiguedad = DateTime.Now.Year - Fechaingreso.Year;
+    }
+    void CalcularEdad(DateTime Fechadenacimiento){
+        edad = DateTime.Now.Year - Fechadenacimiento.Year;
+    }
+
+
+
+
 }
 
 enum Cargos{
