431fb39ff50441a60041b709d5ae0af7ccad1a61
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-06-09 00:37:48 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿using Empleados;
-
-Empleado[] empleado = new(3);
-
-for (int i = 0; i < 3; i++)
-{
-    Console.WriteLine("Ingrese nombre del empleado: ");
-    empleado[i].Nombre = Console.Readline();
-    Console.WriteLine("Ingrese el apellido: ");
-    empleado[i].Apellido = Console.Readline();
-}
+﻿
