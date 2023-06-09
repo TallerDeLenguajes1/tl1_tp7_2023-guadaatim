41f20ccceb6ab1e02ad3837bbcd21b195d5db374
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-06-09 00:31:01 -0300

    cargar empleados

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+
 
 using Empleados;
 
+Empleado[] empleado = new(3);
+
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Ingrese nombre del empleado: ");
+    empleado[i].Nombre = Console.Readline();
+    Console.WriteLine("Ingrese el apellido: ");
+    empleado[i].Apellido = Console.Readline();
+}
